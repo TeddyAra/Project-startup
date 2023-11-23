e89a8f4f84136895effae11ff7553e3dfaf90e70
Jip Verveda <jibril.bear@gmail.com>
2023-11-23 15:00:55 +0100

    Fixed some prototype mechanics
    Added better collisions and removed the possibility of getting stuck on an edge and softlocking yourself,

--- Assets/Scripts/GameScripts/Prototype.cs
+++ Assets/Scripts/GameScripts/Prototype.cs
@@ -13,6 +13,7 @@ public class Prototype : MonoBehaviour {
     [SerializeField] private CharacterController controller;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundMask;
+    [SerializeField] private LayerMask defaultMask;
     [SerializeField] private GameObject canvas;
     [SerializeField] private TMP_Text text;
 
@@ -23,30 +24,21 @@ public class Prototype : MonoBehaviour {
     [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] private float jumpHeight = 1f;
     [SerializeField] private float distanceCheck = 0.1f;
-    [SerializeField] private string deathTileTag = "Death";
+    [SerializeField] private string fallTileTag = "Fall";
     [SerializeField] private string winTag = "Win";
+    [SerializeField] private string deathTag = "Death";
     [SerializeField] private bool camIgnoreX = true;
 
     // Variables
     private Vector3 velocity;
     private bool isGrounded;
     private bool isDead;
+    private bool hasWon;
+    private Vector3 move;
+    private Vector3 fallPosition;
+    private Vector3 fallVelocity;
 
     void Update() {
-        // No input if dead
-        if (isDead) return;
-
-        // Gets input
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        // Applies input        
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (camIgnoreX)
-            cam.position = new Vector3(0f, cam.position.y, cam.position.z);
-
         // Calculates gravity and jump force
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -54,26 +46,56 @@ public class Prototype : MonoBehaviour {
             velocity.y = -2f;
         }
 
-        if (Input.GetButtonDown("Fire1") && isGrounded) {
+        if (Input.GetButtonDown("Fire1") && isGrounded && !isDead) {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
 
+        // Moves player to the center of the fallen tile
+        if (isDead) {
+            fallVelocity = (fallPosition - transform.position).normalized * Time.deltaTime * 4;
+            fallVelocity.y = 0f;
+            velocity += fallVelocity;
+
+            velocity.x *= 1f - Time.deltaTime;
+            velocity.z *= 1f - Time.deltaTime;
+        }
+
         // Applies gravity
         controller.Move(velocity * Time.deltaTime);
 
-        // Checks if player is on a death tile
+        // No input if dead
+        if (isDead || hasWon) return;
+
+        // Gets input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Applies input        
+        move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Checks if player is on a falling tile
         RaycastHit hit;
         if (Physics.Raycast(transform.position, Vector3.down, out hit, distanceCheck, groundMask)) {
-            if (hit.transform.CompareTag(deathTileTag)) {
+            if (hit.transform.CompareTag(fallTileTag)) {
+                fallPosition = hit.transform.position;
+                hit.transform.gameObject.AddComponent<Rigidbody>();
+                hit.transform.gameObject.layer = defaultMask;
                 isDead = true;
-                canvas.SetActive(true);
-            } else if (hit.transform.CompareTag(winTag)) {
-                isDead = true;
-                canvas.SetActive(true);
-                text.SetText("You won!");
-            }
+            } 
+        }
+    }
+
+    // Checks if player should die or win
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag(deathTag)) {
+            canvas.SetActive(true);
+        } else if (other.CompareTag(winTag)) {
+            hasWon = true;
+            text.SetText("You won!");
+            canvas.SetActive(true);
         }
     }
 }
