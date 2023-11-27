c116a316042e9da695fcd17e50ef8925e82b56e2
Jip Verveda <jibril.bear@gmail.com>
2023-11-27 15:38:55 +0100

    Added basic start screen (Host and devices)
    Added a basic start screen where you can put your name in. The C# script checks if the player can join, and the screen on the device is changed.

--- Assets/Scripts/GameScripts/GameLogic.cs
+++ Assets/Scripts/GameScripts/GameLogic.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using NDream.AirConsole;
 using Newtonsoft.Json.Linq;
 using System;
+using TMPro;
 
 public class GameLogic : MonoBehaviour {
     [SerializeField] int maxPlayers = 8;
+    [SerializeField] Canvas startingScreen;
+    [SerializeField] GameObject playerBlock;
+    [SerializeField] GameObject profile;
 
     struct Player {
         public string name; // Name displayed on host screen
@@ -20,12 +24,17 @@ public class GameLogic : MonoBehaviour {
 
     List<Player> players = new List<Player>();
 
+    private bool gameStarted = false;
+    private bool showingCode = false;
+    private int checkPlayerPause = 0;
+
     void Awake() {
         AirConsole.instance.onMessage += OnMessage;
     }
 
     // Receives messages
     void OnMessage(int fromDeviceID, JToken data) {
+        JToken message;
         Debug.Log("Message received from " + fromDeviceID);
 
         // A player wants to join the game
@@ -34,32 +43,56 @@ public class GameLogic : MonoBehaviour {
 
             // There are too many players
             if (players.Count >= maxPlayers) {
-                SendMessage(fromDeviceID, "max");
+                message = JToken.Parse(@"{'type':'message','message':'too-many'}");
+                SendMessage(fromDeviceID, message);
                 Debug.Log("Too many players");
+                return;
+            }
+
+            // Name empty
+            if (data["join"].ToString().Length < 1) {
+                message = JToken.Parse(@"{'type':'message','message':'not-valid'}");
+                SendMessage(fromDeviceID, message);
+                Debug.Log("Name empty");
                 return;
             }
 
             // There is already someone with that name
             foreach (Player player in players) {
                 if (data["join"].ToString().Equals(player.name)) {
-                    SendMessage(fromDeviceID, "name");
+                    message = JToken.Parse(@"{'type':'message','message':'taken'}");
+                    SendMessage(fromDeviceID, message);
                     Debug.Log("Already someone with that name");
                     return;
                 }
             }
 
             // Player can join the game
-            SendMessage(fromDeviceID, "joined");
+            message = JToken.Parse(@"{'type':'change','screen':'join-screen'}");
+            SendMessage(fromDeviceID, message);
+
+            message = JToken.Parse(@"{'type':'message','screen':'joined'}");
+            SendMessage(fromDeviceID, message);
+
             AddPlayer(data["join"].ToString(), fromDeviceID);
             return;
         }
     }
 
     void AddPlayer(string name, int id) {
+        // Creates a profile on UI
+        GameObject newProfile = Instantiate(profile);
+        newProfile.transform.parent = playerBlock.transform;
+        Vector3 newPosition = newProfile.GetComponent<RectTransform>().anchoredPosition;
+        newPosition.y = 365 - players.Count * 75;
+        newProfile.GetComponent<RectTransform>().anchoredPosition = new Vector3(25, 350 - players.Count * 100, 0);
+        newProfile.GetComponent<TMP_Text>().text = name;
+
+        // Creates a player struct
         Player newPlayer = new Player(name, id);
         players.Add(newPlayer);
 
-        Debug.Log(newPlayer.name + " joined!");
+        Debug.Log(name + " joined!");
     }
 
     // Sends a message to one device
@@ -82,6 +115,19 @@ public class GameLogic : MonoBehaviour {
                 AirConsole.instance.Message(id, msg);
             }
         }
+    }
+
+    void Update() { 
+        /*if (!gameStarted) {
+            checkPlayerPause += 1;
+            if (AirConsole.instance.IsAirConsoleUnityPluginReady() && checkPlayerPause > 30) {
+                if (!showingCode) {
+                    // Show code here!
+                    showingCode = true;
+                }
+                checkPlayerPause = 0;
+            }
+        }*/
     }
 
 
