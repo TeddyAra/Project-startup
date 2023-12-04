9c861e8218b55823ae2aa2497176309628f69ff1
Jip Verveda <jibril.bear@gmail.com>
2023-12-04 13:29:08 +0100

    Added comments for branch script

--- Assets/Scripts/GameScripts/BranchesScript.cs
+++ Assets/Scripts/GameScripts/BranchesScript.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using UnityEngine;
 
 public class BranchesScript : MonoBehaviour {
-    [SerializeField] GameObject branch;
-    [SerializeField] float spawnTime;
-    [SerializeField] float spawnHeight;
-    [SerializeField] float spawnRange;
-    [SerializeField] float branchLifeTime;
-    [SerializeField] float spawnDistance;
+    [SerializeField] GameObject branch; // Branch prefab
+    [SerializeField] float spawnTime; // After how much time should a branch be spawned
+    [SerializeField] float spawnHeight; // How high up should the branch spawn
+    [SerializeField] float spawnRange; // How far away can the branch spawn
+    [SerializeField] float branchLifeTime; // For how long should the branch exist
+    [SerializeField] float spawnDistance; // How far forward should the branch spawn
 
     private float timer;
     private Dictionary<GameObject, float> branches;
@@ -19,24 +19,30 @@ public class BranchesScript : MonoBehaviour {
     }
 
     void Update() {
+        // Adds time to timer and branch lifetime
         timer += Time.deltaTime;
 
         foreach (var item in branches.Keys.ToList()) {
             branches[item] += Time.deltaTime;
+            // Remove branch
             if (branches[item] > branchLifeTime) {
                 branches.Remove(item);
                 Destroy(item);
             }
         }
 
+        // Spawn a new branch
         if (timer > spawnTime) {
+            // Reset timer and get the base position
             timer = 0f;
             Vector3 position = transform.position;
             position.y = spawnHeight;
 
-            //position.x += Random.Range(-spawnRange, spawnRange);
-            //position.z += Random.Range(-spawnRange, spawnRange) + spawnDistance;
+            // Randomize position of branch and move it forward
+            position.x += Random.Range(-spawnRange, spawnRange);
+            position.z += Random.Range(-spawnRange, spawnRange) + spawnDistance;
 
+            // Create the branch and add it to the dictionary
             GameObject newBranch = Instantiate(branch, position, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
             branches.Add(newBranch, 0);
         }
